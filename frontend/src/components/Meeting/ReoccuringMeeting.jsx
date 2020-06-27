f729684166cac00c3e2ef58e638bf71f507784f0
jackfales <jackfales@gmail.com>
2020-06-27 14:40:43 -0700

    added basic jsx for meeting title and time inputs

--- frontend/src/components/Meeting/ReoccuringMeeting.jsx
+++ frontend/src/components/Meeting/ReoccuringMeeting.jsx
@@ -6,8 +6,22 @@ export default (props) => {
 
   return (
     <div>
-      <h1>ReoccuringMeeting Component</h1>
+      <form>
+        <h1>Reoccuring Meeting</h1>
+          <div id="meeting-input">
+            <h2>Meeting Title</h2>
+            <input type="text" placeholder="Meeting Title" />
+          </div>
+          <div id="time-input">
+            <label for="start-time">Start</label>
+            <input type="time" id="start-time" value="09:00" />
+            <label for="end-time">End</label>
+            <input type="time" id="end-time" value="17:00" />
+          </div>
+          <div id="time-available">
+            <h2>Select the days available for your meeting</h2>
+          </div>
+      </form>
     </div>
-
   );
 };
