41c36542abbd5a9e956d4755d087c4464349af3a
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-06-27 19:08:35 -0700

    add jsx. todo: right align About + other links.

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,22 +1,31 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import {
-  Navbar, Nav, Row, Col,
-} from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import './Main.css';
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { Navbar, Nav, Row, Col } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Main.css";
 // Styles for react-date-range
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import {
-  OneTimeMeeting, About, ReoccuringMeeting, NameModal,
-} from '../components';
+  OneTimeMeeting,
+  About,
+  ReoccuringMeeting,
+  NameModal,
+} from "../components";
 
 class Main extends Component {
   render() {
     return (
+      // to do: figure out styling to align to right
       <div>
+        <nav class="navbar navbar-light bg-light justify-content-between">
+          <a class="navbar-brand" href="/">
+            <h4>When4Meet</h4>
+          </a>
+          <a href="/about" className="nav-item mr-sm-2">About</a>
+          <a href="/about" className="nav-item ">Another Link</a>
+        </nav>
 
         <Switch>
           <Route
@@ -32,7 +41,6 @@ class Main extends Component {
           <Route path="/meeting/:id" render={() => <NameModal />} />
           <Route path="/about" render={() => <About />} />
         </Switch>
-
       </div>
     );
   }
