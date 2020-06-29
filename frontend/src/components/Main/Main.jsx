50b2e955c0909ae657317193af3f16b159a8187b
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-06-28 22:43:01 -0700

    move styling to Main.css

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,28 +1,25 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import './Main.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Main.css";
 // Styles for react-date-range
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import {
   OneTimeMeeting,
   About,
   ReoccuringMeeting,
   NameModal,
-} from '../components';
+} from "../components";
 
 class Main extends Component {
   render() {
     return (
       <div>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand
-            href="/"
-            style={{ fontWeight: 500, fontSize: '20px', lineHeight: '17px' }}
-          >
+          <Navbar.Brand href="/" className="nav-branding">
             When4Meet
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
