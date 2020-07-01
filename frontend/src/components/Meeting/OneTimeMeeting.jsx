31574d7e51f03331fbcbf464120204299b6d7191
Cole <ctperry0301@gmail.com>
2020-07-01 01:36:24 -0500

    Converted to bootstrap components

--- frontend/src/components/Meeting/OneTimeMeeting.jsx
+++ frontend/src/components/Meeting/OneTimeMeeting.jsx
@@ -1,61 +1,71 @@
 import React, { useState } from 'react';
 import { DateRange } from 'react-date-range';
+import { addDays } from 'date-fns';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 export default () => {
   console.log('Rendering OneTimeMeeting');
   const [state, setState] = useState([
     {
       startDate: new Date(),
-      endDate: null,
+      endDate: addDays(new Date(), 7),
       key: 'selection',
     },
   ]);
 
   return (
-    <div style={{ border: '2px solid black', padding: '20px', width: '35%' }}>
-      <h2>One-Time Meeting </h2>
-      <form>
-        <span> Meeting Title</span>
-        <input
-          className="input-group mt-1 mb-2"
-          style={{ width: '100%' }}
-          placeholder="Meeting Title"
-        />
-        <span>
-          <span> Start </span>
-          <input
-            className="ml-1 mr-3"
-            style={{ width: '35%' }}
-            placeholder="9:00 am"
-          />
-          <span> End </span>
-          <input
-            className="ml-1"
-            style={{ width: '35%' }}
-            placeholder="5:00 pm"
-          />
-        </span>
-        <center>
-          <p className="mt-2">
-            Select the potential dates for yout meeting
-          </p>
-          <DateRange
-            editableDateInputs
-            onChange={(item) => setState([item.selection])}
-            moveRangeOnFirstSelection={false}
-            ranges={state}
-          />
-        </center>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={console.log('Submitted')}
-        >
-          Create Event
-        </button>
+    <div style={{ borderRadius: '20px', backgroundColor: '#EFEFEF', padding: '24px' }}>
+      <h3>One-Time Meeting </h3>
+      <Form>
+        <Form.Group>
+          <Form.Label className="font-weight-bold">Meeting Title</Form.Label>
+          <Form.Control placeholder="Meeting Title" />
+        </Form.Group>
 
-      </form>
+        <Form.Group>
+          <Form.Row>
+            <Form.Label className="font-weight-bold mt-1">Start</Form.Label>
+            <Col>
+              <Form.Control placeholder="9:00 am" />
+            </Col>
+            <Form.Label className="font-weight-bold mt-1">End</Form.Label>
+            <Col>
+              <Form.Control placeholder="5:00 pm" />
+            </Col>
+          </Form.Row>
+        </Form.Group>
+
+        <Form.Row className="justify-content-center">
+          <Form.Group>
+            <Form.Label className="font-weight-bold ml-4 mb-n1">Select the potential dates for your meeting</Form.Label>
+            <Col>
+              <DateRange
+                editableDateInputs
+                onChange={(item) => setState([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={state}
+              />
+            </Col>
+          </Form.Group>
+        </Form.Row>
+
+        <Button className="primary" type="submit">Create Event</Button>
+
+      </Form>
     </div>
 
   );
 };
+/*
+<form>
+  <button
+    type="submit"
+    className="btn btn-primary"
+    onClick={console.log('Submitted')}
+  >
+    Create Event
+  </button>
+</form>
+*/
