f758072d11a90ca06c33c83623432879352c8f79
jackfales <jackfales@gmail.com>
2020-07-04 11:17:00 -0700

    Added time input list, changed ui container styling to inline styling

--- frontend/src/components/Meeting/ReoccuringMeeting.jsx
+++ frontend/src/components/Meeting/ReoccuringMeeting.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import {
-  Button, Container, Col, Form,
+  Button, Col, Form,
 } from 'react-bootstrap';
 import './ReoccuringMeeting.css';
+import { START_OPTIONS, END_OPTIONS } from './MeetingConstants';
 
 export default () => {
   console.log('Rendering ReoccuringMeeting');
@@ -54,7 +55,7 @@ export default () => {
   };
 
   return (
-    <Container id="ui-container" fluid>
+    <div style={{ borderRadius: '20px', backgroundColor: '#EFEFEF', padding: '24px' }}>
       <h3>Reoccuring Meeting</h3>
       <Form>
         {/* meeting title input */}
@@ -69,109 +70,13 @@ export default () => {
             <Form.Label className="font-weight-bold mt-1">Start</Form.Label>
             <Col>
               <Form.Control controlId="startTime" as="select" placeholder="9:00 am">
-                <option value=""> </option>
-                <option value="12:00am"> 12:00am </option>
-                <option value="12:30am"> 12:30am </option>
-                <option value="1:00am"> 1:00am </option>
-                <option value="1:30am"> 1:30am </option>
-                <option value="2:00am"> 2:00am </option>
-                <option value="2:30am"> 2:30am </option>
-                <option value="3:00am"> 3:00am </option>
-                <option value="3:30am"> 3:30am </option>
-                <option value="4:00am"> 4:00am </option>
-                <option value="4:30am"> 4:30am </option>
-                <option value="5:00am"> 5:00am </option>
-                <option value="5:30am"> 5:30am </option>
-                <option value="6:00am"> 6:00am </option>
-                <option value="6:30am"> 6:30am </option>
-                <option value="7:00am"> 7:00am </option>
-                <option value="7:30am"> 7:30am </option>
-                <option value="8:00am"> 8:00am </option>
-                <option value="8:30am"> 8:30am </option>
-                <option value="9:00am"> 9:00am </option>
-                <option value="9:30am"> 9:30am </option>
-                <option value="10:00am"> 10:00am </option>
-                <option value="10:30am"> 10:30am </option>
-                <option value="11:00am"> 11:00am </option>
-                <option value="11:30am"> 11:30am </option>
-                <option value="12:00pm"> 12:00pm </option>
-                <option value="12:30pm"> 12:30pm </option>
-                <option value="1:00pm"> 1:00pm </option>
-                <option value="1:30pm"> 1:30pm </option>
-                <option value="2:00pm"> 2:00pm </option>
-                <option value="2:30pm"> 2:30pm </option>
-                <option value="3:00pm"> 3:00pm </option>
-                <option value="3:30pm"> 3:30pm </option>
-                <option value="4:00pm"> 4:00pm </option>
-                <option value="4:30pm"> 4:30pm </option>
-                <option value="5:00pm"> 5:00pm </option>
-                <option value="5:30pm"> 5:30pm </option>
-                <option value="6:00pm"> 6:00pm </option>
-                <option value="6:30pm"> 6:30pm </option>
-                <option value="7:00pm"> 7:00pm </option>
-                <option value="7:30pm"> 7:30pm </option>
-                <option value="8:00pm"> 8:00pm </option>
-                <option value="8:30pm"> 8:30pm </option>
-                <option value="9:00pm"> 9:00pm </option>
-                <option value="9:30pm"> 9:30pm </option>
-                <option value="10:00pm"> 10:00pm </option>
-                <option value="10:30pm"> 10:30pm </option>
-                <option value="11:00pm"> 11:00pm </option>
-                <option value="11:30pm"> 11:30pm </option>
+                {START_OPTIONS}
               </Form.Control>
             </Col>
             <Form.Label className="font-weight-bold mt-1">End</Form.Label>
             <Col>
               <Form.Control controlId="endTime" as="select" placeholder="5:00 pm">
-                <option value=""> </option>
-                <option value="12:00am"> 12:00am </option>
-                <option value="12:30am"> 12:30am </option>
-                <option value="1:00am"> 1:00am </option>
-                <option value="1:30am"> 1:30am </option>
-                <option value="2:00am"> 2:00am </option>
-                <option value="2:30am"> 2:30am </option>
-                <option value="3:00am"> 3:00am </option>
-                <option value="3:30am"> 3:30am </option>
-                <option value="4:00am"> 4:00am </option>
-                <option value="4:30am"> 4:30am </option>
-                <option value="5:00am"> 5:00am </option>
-                <option value="5:30am"> 5:30am </option>
-                <option value="6:00am"> 6:00am </option>
-                <option value="6:30am"> 6:30am </option>
-                <option value="7:00am"> 7:00am </option>
-                <option value="7:30am"> 7:30am </option>
-                <option value="8:00am"> 8:00am </option>
-                <option value="8:30am"> 8:30am </option>
-                <option value="9:00am"> 9:00am </option>
-                <option value="9:30am"> 9:30am </option>
-                <option value="10:00am"> 10:00am </option>
-                <option value="10:30am"> 10:30am </option>
-                <option value="11:00am"> 11:00am </option>
-                <option value="11:30am"> 11:30am </option>
-                <option value="12:00pm"> 12:00pm </option>
-                <option value="12:30pm"> 12:30pm </option>
-                <option value="1:00pm"> 1:00pm </option>
-                <option value="1:30pm"> 1:30pm </option>
-                <option value="2:00pm"> 2:00pm </option>
-                <option value="2:30pm"> 2:30pm </option>
-                <option value="3:00pm"> 3:00pm </option>
-                <option value="3:30pm"> 3:30pm </option>
-                <option value="4:00pm"> 4:00pm </option>
-                <option value="4:30pm"> 4:30pm </option>
-                <option value="5:00pm"> 5:00pm </option>
-                <option value="5:30pm"> 5:30pm </option>
-                <option value="6:00pm"> 6:00pm </option>
-                <option value="6:30pm"> 6:30pm </option>
-                <option value="7:00pm"> 7:00pm </option>
-                <option value="7:30pm"> 7:30pm </option>
-                <option value="8:00pm"> 8:00pm </option>
-                <option value="8:30pm"> 8:30pm </option>
-                <option value="9:00pm"> 9:00pm </option>
-                <option value="9:30pm"> 9:30pm </option>
-                <option value="10:00pm"> 10:00pm </option>
-                <option value="10:30pm"> 10:30pm </option>
-                <option value="11:00pm"> 11:00pm </option>
-                <option value="11:30pm"> 11:30pm </option>
+                {END_OPTIONS}
               </Form.Control>
             </Col>
           </Form.Row>
@@ -213,8 +118,8 @@ export default () => {
             </Form.Row>
           </Form.Group>
         </Form.Row>
-        <Button variant="primary">Create Event</Button>
+        <Button className="primary" type="submit">Create Event</Button>
       </Form>
-    </Container>
+    </div>
   );
 };
