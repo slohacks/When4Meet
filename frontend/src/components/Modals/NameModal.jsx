1bf5c410a7aa15aa3ab15488a4347092802afc00
Shriya <shriyan44@gmail.com>
2020-07-03 20:37:24 -0700

    final fixes on dev5

--- frontend/src/components/Modals/NameModal.jsx
+++ frontend/src/components/Modals/NameModal.jsx
@@ -3,41 +3,39 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './NameModal.css'
 
-// 20px and a background color of #EFEFEF to match the mockups.
 
 export default (props) => {
   console.log('Rendering NameModal');
 
+  const [state, setState] = React.useState({
+    showModal: true
+  });
+
   return (
 
-      <Modal.Dialog>
-        {/* <div className="modal-content"> */}
-          <div className="modal-background-color">
-        
+      <Modal show={state.showModal} id= "modalcont">
+        <div className="modal-background-color">
 
+          <Modal.Body>
+            
+            <Modal.Title className="margins text-center">Meeting Title</Modal.Title>
+              <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                  <Form.Label><h5>What's your name?</h5></Form.Label>
+                  <Form.Control type="name" placeholder="Name" />
+                </Form.Group>
+              </Form>
 
-      <Modal.Body>
-        
-        <Modal.Title><h4 className="text-center">Meeting Title</h4></Modal.Title>
-          <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label><h5>What's your name?</h5></Form.Label>
-              <Form.Control type="name" placeholder="Name" />
-            </Form.Group>
-          </Form>
+              <Button variant="primary" size="sm" onClick={() => setState({showModal: false})}>
+                Continue
+              </Button>
+            
+          </Modal.Body>
 
-          <Button variant="primary" size="sm">
-            Continue
-          </Button>{' '}
-        
-      </Modal.Body>
-
-        {/* </div> */}
         </div>
-      </Modal.Dialog>
+      </Modal>
     
 
   );
