c6050ebd790c4bbb068a49dbaee48ce1a6944e6b
jpoist97 <jpoist97@gmail.com>
2020-07-10 22:33:50 -0700

    [DEV-11] Setup Database
    Setup mysql database and fleshed out GET/PUT endpoints for Meeting. Also put into place things for DEV-12 and DEV-13.

--- frontend/src/actions/actionCreators.js
+++ frontend/src/actions/actionCreators.js
@@ -1,1 +1,8 @@
-// import * as api from '../api';
+/* eslint-disable */
+import * as api from '../api';
+
+export function updateMeeting(meetingId, cb) {
+  return (dispatch, prevState) => {
+    console.log('Updating Meeting');
+  };
+}
---
+++ frontend/src/reducers/Meeting.js
@@ -0,0 +1,9 @@
+export default function Meeting(state = [], action) {
+  switch (action.type) {
+    case 'LOAD_MEETING':
+      // UPDATE STORE HERE
+      break;
+    default:
+      return state;
+  }
+}
