ef4c6208c2665a011c8d60970f8da8ec0eda4b05
jackfales <jackfales@gmail.com>
2020-07-25 11:20:46 -0700

    Changed test data to start and end time; installed moment; fixed linting

--- frontend/src/components/Viewer/Viewer.jsx
+++ frontend/src/components/Viewer/Viewer.jsx
@@ -1,50 +1,60 @@
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
 import React from 'react';
 import Popup from 'reactjs-popup';
 import { Row } from 'react-bootstrap';
 import './Viewer.css';
 
+const moment = require('moment');
+
 export default () => {
   console.log('Rendering Viewer');
 
+  moment().format();
+
   // test data
+  const startTime = '9:00am';
+  const endTime = '5:00pm';
   const days = ['Mon', 'Wed', 'Fri', 'Sat'];
-  const times = [
-    '9:00am',
-    '9:30am',
-    '10:00am',
-    '10:30am',
-    '11:00am',
-    '11:30am',
-    '10:30am',
-    '11:00am',
-    '11:30am',
-    '11:00am',
-    '11:30am',
-    '10:30am',
-    '11:00am',
-    '11:30am',
-    '11:00am',
-    '11:30am',
-  ];
+
+  const startmil = moment(startTime, 'hh:mm A').format('HH:mm');
+  const startt = moment(startmil, 'HH:mm');
+
+  const endmil = moment(endTime, 'hh:mm A').format('HH:mm');
+  const endt = moment(endmil, 'HH:mm');
+
+  const hours = [];
+  const duration = (endt.diff(startt, 'hours'));
+  const endHour = parseInt(duration, 10);
+  const startHour = startTime.slice(0, 4);
+
+  for (let hour = 0; hour < endHour + 1; hour++) {
+    hours.push(
+      moment({ hour })
+        .add(startHour, 'hours')
+        .format('h:mm A'),
+    );
+    hours.push(
+      moment({
+        hour,
+        minute: 30,
+      })
+        .add(startHour, 'hours')
+        .format('h:mm A'),
+    );
+  }
 
   return (
     <div className="selector-content">
-      {/* create a row for each time in the times array */}
-      {times.map((time) => (
+      {hours.map((time) => (
         <Row>
           <div className="time-label">{time}</div>
-          {/* create a column for each day in the days array */}
           {days.map((day) => (
-            // create a popup for every button that displays information
-            // corresponding to who can attend a such meeting at the time given
-            // by an aformentioned label.
             <Popup trigger={<div aria-label={day} className="cell cell-disabled" />} position="left center" on="hover">
               {time}
             </Popup>
           ))}
         </Row>
       ))}
-      {/* create a row that displays labels on the "x axis" of the grid. */}
       <Row>
         <div className="time-label" />
         {days.map((dayname) => (
